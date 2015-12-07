69fba3c7f3a1ed6482e1791963f93b772d4c9daa
Paulo <paulormartins12@gmail.com>
2015-12-07 01:33:16 -0200

    Update assignment_week4.py

--- assignment_week4.py
+++ assignment_week4.py
@@ -1,1 +1,14 @@
 
+"""
+Write a program that uses raw_input to prompt a user for their name and then welcomes them. 
+Note that raw_input will pop up a dialog box. 
+Enter Sarah in the pop-up box when you are prompted so your output will match the desired output.
+"""
+
+inp = raw_input ("Enter hours: ")
+hours = float (inp)
+inp = raw_input ("Enter rate: ")
+rate = float (inp)
+
+pay = rate * hours
+print pay
